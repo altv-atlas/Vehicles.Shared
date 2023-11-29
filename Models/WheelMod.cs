d9422f4640bea3890de4efc2042789c672ce1e79
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-11-29 09:08:53 +0100

    initial commit

---
+++ Models/WheelMod.cs
@@ -0,0 +1,22 @@
+﻿namespace AltV.Atlas.Vehicles.Shared.Models;
+
+/// <summary>
+/// Object that stores wheel modification data
+/// </summary>
+public class WheelMod
+{
+    public byte Index { get; set; }
+
+    public float Camber { get; set; }
+
+    public float Height { get; set; }
+    
+    public float RimRadius { get; set; }
+    
+    public float TrackWidth { get; set; }
+    
+    public float TyreRadius { get; set; }
+    
+    public float TyreWidth { get; set; }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Vehicles.Shared
+Shared vehicles module for the Atlas project
+
+## Documentation
+Please refer to our [official docs](https://altv-atlas.github.io/docs/index.html) for more information.
