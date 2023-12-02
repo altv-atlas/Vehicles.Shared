8af5614a45a774bd56fdf3f58e936cf217ff0b85
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2023-12-02 14:23:44 +0100

    -added xml docs

--- Models/WheelMod.cs
+++ Models/WheelMod.cs
@@ -5,18 +5,38 @@
 /// </summary>
 public record WheelMod
 {
+    /// <summary>
+    /// The vehicle wheel index
+    /// </summary>
     public byte Index { get; set; }
-
+    
+    /// <summary>
+    /// The wheels camber value
+    /// </summary>
     public float Camber { get; set; }
-
+    
+    /// <summary>
+    /// The wheels height value
+    /// </summary>
     public float Height { get; set; }
-    
-    public float RimRadius { get; set; }
-    
-    public float TrackWidth { get; set; }
-    
-    public float TyreRadius { get; set; }
-    
-    public float TyreWidth { get; set; }
 
+    /// <summary>
+    /// The wheels rimRadius value
+    /// </summary>
+    public float RimRadius { get; set; }
+
+    /// <summary>
+    /// The wheels trackWidth value
+    /// </summary>
+    public float TrackWidth { get; set; }
+
+    /// <summary>
+    /// The wheels tyreRadius value
+    /// </summary>
+    public float TyreRadius { get; set; }
+
+    /// <summary>
+    /// The wheels tyreWidth
+    /// </summary>
+    public float TyreWidth { get; set; }
 }
--- VehicleConstants.cs
+++ VehicleConstants.cs
@@ -1,6 +1,12 @@
 ﻿namespace AltV.Atlas.Vehicles.Shared;
 
+/// <summary>
+/// A static class containing all vehicle related constants
+/// </summary>
 public static class VehicleConstants
 {
+    /// <summary>
+    /// The value for the changeWheels meta
+    /// </summary>
     public const string ChangeWheelsMetaKey = "atlas:vehicles:changeWheels"; 
 }
